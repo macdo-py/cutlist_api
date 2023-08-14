69249a11775f51a3778474e0c5d4ad03b7273fbd
macdo-py <54476230+macdo-py@users.noreply.github.com>
2023-08-14 10:42:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
-import fastapi
-app = fastapi.FastAPI()
+import os
+import json
 
-@app.get("/")
-def hello_world():
-    return "Hello, world!"
+def handler(event, context):
+  """Vercel serverless function handler."""
+
+  # Get the event data from the request body.
+  event_data = json.loads(event['body'])
+
+  # Do something with the event data.
+  print(event_data)
+
+  # Return a response to the client.
+  response = {
+    'statusCode': 200,
+    'body': json.dumps({'message': 'Hello, world!'})
+  }
+
+  return response
