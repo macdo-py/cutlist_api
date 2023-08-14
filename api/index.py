8a324a73a2d50c149688392d2fe3ccad42bcd1b1
macdo-py <54476230+macdo-py@users.noreply.github.com>
2023-08-14 11:27:24 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,19 +1,10 @@
-#import os
-import json
+from http.server import BaseHTTPRequestHandler
 
-def handler(event, context):
-  """Vercel serverless function handler."""
 
-  # Get the event data from the request body.
-  event_data = json.loads(event['body'])
-
-  # Do something with the event data.
-  print(event_data)
-
-  # Return a response to the client.
-  response = {
-    'statusCode': 200,
-    'body': json.dumps({'message': 'Hello, world!'})
-  }
-
-  return response
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str('Hello World!!').encode())
+        return
