3edf84ff6ad491b1c038b0233b6524e20f14284e
macdo-py <54476230+macdo-py@users.noreply.github.com>
2023-08-22 17:10:00 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
-from http.server import BaseHTTPRequestHandler
-#to push
-class handler(BaseHTTPRequestHandler):
+from sanic import Sanic
+from sanic.response import json
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        response = {'message': 'Hello, world!'}
-        self.wfile.write(json.dumps(response).encode('utf-8'))
-        return
+app = Sanic(__name__)
+
+def fibonacci_ratio(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_ratio(n-1) / fibonacci_ratio(n-2)
+
+@app.route('/fibonacci-ratio/<n:int>')
+async def fibonacci_ratio_handler(request, n):
+    ratio = fibonacci_ratio(n)
+    return json({'ratio': ratio})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
